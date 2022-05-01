cee4971e7642109be0641d6c52db9819894bfe57
Canofcode <kurtisdenning@gmail.com>
2022-05-01 15:01:27 +1200

    Added nav and router components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Homepage from "./Pages/Home";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
+import Router from "./Components/Router";
+import Nav from "./Components/Nav";
+import "./style.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route exact path="/" element={<Homepage />}></Route>
-
-        <Route exact path="/about" element={<About />}></Route>
-
-        <Route exact path="/contact" element={<Contact />}></Route>
-      </Routes>
+    <div>
+      <Nav />
+      <Router />
     </div>
   );
 };
---
+++ src/Components/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "../Pages/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Homepage />}></Route>
+
+      <Route exact path="/about" element={<About />}></Route>
+
+      <Route exact path="/contact" element={<Contact />}></Route>
+    </Routes>
+  );
+};
+
+export default Router;
