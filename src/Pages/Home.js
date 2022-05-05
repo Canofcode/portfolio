3772b3caa2bae5b1f83397c7f76b69ddc1785409
Kurtis Denning <kurtisdenning@gmail.com>
2022-05-05 23:22:30 +1200

    Finished projects I think

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../style.css"
 import pp from "../Images/pp.jpg"
 import sp from "../Images/sp.jpg"
+import sp2 from "../Images/sp2.jpg"
 import btnArrow from "../Images/btnArrow.png"
 import { Col, Row, Image, Container, Button } from "react-bootstrap";
 
@@ -36,7 +37,7 @@ const Home = () => {
           </Col>
         </Row>
         <div className="relcontain">
-          <Image className="img-fluid image-bottom-faide" src={sp} />
+          <Image className="img-fluid" src={sp} />
           <Button
             style={{ background: "#55508e", color: "white", fontWeight: "bold" }} className="pos-top-right ps-3 pe-3 pt-2 pb-2 ps-md-4 pe-md-4 pt-md-3 pb-md-3">
             <img src={btnArrow} alt="btnArrow" className=" btnArrow" />
@@ -45,6 +46,21 @@ const Home = () => {
           <p className="me-2 mb-2 home--proj-date home--proj-name pos-bottom-right">2018 - Current</p>
         </div>
       </div>
+
+      <Row className="pt-5 mt-5">
+        <Col className="p-0" md={{ span: 7, offset: 0 }}>
+          <Image fluid src={sp2}></Image>
+        </Col>
+        <Col className="m-0 p-0" md={{ span: 5, offset: 0 }}>
+          <div className="home--proj-two-text d-flex relcontain">
+            <p className="ps-5 ms-4 home--proj-date home--proj-name home--date-two purple ">2022 - Current</p>
+            <p className="ms-md-5 pt-md-3 pt-5 home--proj-name home--proj-name-two purple">This Portfolio!</p>
+            <div className="purple pos-bottom-right w-75 pb-5 me-4">
+              <p>The project that I am currently working on which uses React plus bootstrap 5.1. This is an on going project that I will be keeping up to date with my new projects and achievements.</p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </Container>
   )
 };
